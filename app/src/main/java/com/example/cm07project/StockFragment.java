fb99a8bce9bf878f0cbf3d79c27119cf5eb02484
hugo27rocha <hugo27rocha@gmail.com>
2021-12-15 17:28:51 +0000

    Simple Market

--- app/src/main/java/com/example/cm07project/StockFragment.java
+++ app/src/main/java/com/example/cm07project/StockFragment.java
@@ -2,18 +2,121 @@ package com.example.cm07project;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class StockFragment extends Fragment {
+
+    private ListView listView;
+    private FirebaseUser user;
+    private DatabaseReference reference;
+
+    private EditText edit;
+    private Button addd;
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_stock, container, false);
+
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_stock, null);
+        edit = root.findViewById(R.id.edit);
+        addd = root.findViewById(R.id.addd);
+
+       // FirebaseDatabase.getInstance().getReference("Market").child("3").setValue("asb");
+        addd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String txt_name = edit.getText().toString();
+                FirebaseDatabase.getInstance().getReference("Market").push().setValue(txt_name);
+            }
+        });
+
+
+        ListView listView = (ListView) root.findViewById(R.id.listView);
+        final ArrayList<String> list = new ArrayList<>();
+        final ArrayAdapter adapter = new ArrayAdapter<String>(root.getContext(), R.layout.simple_list_item_1, list);
+        listView.setAdapter(adapter);
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Market");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                list.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    list.add(snapshot.getValue().toString());
+                }
+
+                adapter.notifyDataSetChanged();
+
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+/**
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Market");
+        reference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                String value=snapshot.getValue(String.class);
+                arrayList.add(value);
+                arrayAdapter = new ArrayAdapter<String>(StockFragment.this, R.layout.simple_list_item_1, arrayList);
+                listView.setAdapter(arrayAdapter);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        }); **/
+
+
+
+
+        //return inflater.inflate(R.layout.fragment_stock, container, false);
+        return root;
     }
 }
